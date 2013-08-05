496344851b1decd96b102f0e180c7e0fe7350f74
Fabien Potencier <fabien.potencier@gmail.com>
2013-08-05 18:31:25 +0200

    emptied deprecated interfaces

--- lib/Twig/NodeInterface.php
+++ lib/Twig/NodeInterface.php
@@ -17,14 +17,4 @@
  */
 interface Twig_NodeInterface extends Countable, IteratorAggregate
 {
-    /**
-     * Compiles the node to PHP.
-     *
-     * @param Twig_Compiler A Twig_Compiler instance
-     */
-    public function compile(Twig_Compiler $compiler);
-
-    public function getLine();
-
-    public function getNodeTag();
 }
--- lib/Twig/ParserInterface.php
+++ lib/Twig/ParserInterface.php
@@ -17,12 +17,4 @@
  */
 interface Twig_ParserInterface
 {
-    /**
-     * Converts a token stream to a node tree.
-     *
-     * @param Twig_TokenStream $stream A token stream instance
-     *
-     * @return Twig_Node_Module A node tree
-     */
-    public function parse(Twig_TokenStream $stream);
 }
